abbdc3426ae1070d853a0b8fe9bd3f04da8b491e
Michał Manios <michal.manios83@gmail.com>
2022-11-09 20:20:13 +0100

    keycloak ready before changes to my

--- src/main/java/pl/gov/listaobecnosci/ListaObecnosciApplication.java
+++ src/main/java/pl/gov/listaobecnosci/ListaObecnosciApplication.java
@@ -2,8 +2,10 @@ package pl.gov.listaobecnosci;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true,  securedEnabled = true,  jsr250Enabled = true)
 public class ListaObecnosciApplication {
 
 public static void main(String[] args) {SpringApplication.run(ListaObecnosciApplication.class, args);}
---
+++ src/main/java/pl/gov/listaobecnosci/security/config/ProjectSecurityConfig.java
@@ -0,0 +1,47 @@
+package pl.gov.listaobecnosci.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .cors().configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setExposedHeaders(List.of("Authorization"));
+                    config.setMaxAge(3600L);
+                    return config;
+                }).and().csrf().ignoringRequestMatchers(new AntPathRequestMatcher("/")).csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers(new AntPathRequestMatcher("/api/freedays")).hasAnyRole("AUTHORIZED_USER","ADMINISTRATOR")
+                .requestMatchers(new AntPathRequestMatcher("/api/sections")).hasAnyRole("AUTHORIZED_USER","ADMINISTRATOR")
+                .requestMatchers(new AntPathRequestMatcher("/api/roles")).hasRole("ADMINISTRATOR")
+                .requestMatchers(new AntPathRequestMatcher("/api/users")).hasRole("ADMINISTRATOR")
+                .requestMatchers(new AntPathRequestMatcher("/api/workers")).hasAnyRole("AUTHORIZED_USER","ADMINISTRATOR")
+                .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                .and().oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter);
+        return http.build();
+    }
+}
